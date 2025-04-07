d6edd6fd8328b1af7c51618e19b3fc78d314dcce
sysmae <j970330@gmail.com>
2025-04-07 13:48:43 +0900

    Co-authored-by: gyeonghun kim <ggyhun@users.noreply.github.com>

--- calculator/calculate.py
+++ calculator/calculate.py
@@ -18,7 +18,7 @@ def get_float_input(prompt):
             return float(input(prompt))
         except ValueError:
             print("숫자가 아닙니다. 다시 입력하세요.")
-
+ 
 if __name__ == '__main__':
     while True:
         # 사용자 입력
@@ -40,10 +40,23 @@ if __name__ == '__main__':
             result = divide(num1, num2)
         else:
             result = "Error: Invalid operation"
-
+       
         print(f'사칙연산 결과는 {result}입니다.')
-
         # 계속할지 여부 확인
         cont = input("\n계속하시겠습니까? (y/n): ")
         if cont.lower() != 'y':
             break
+
+# def copilot_test():
+#     화면이 휙휙 바껴요
+#     저희 노션 워크스페이스도 마
+
+# 근데 이거 나중에 쓸까요? 신기하긴 한데
+# 보통 일찍 끝내주시지 않나요? 끝나고 시간 있으면 조금 해봐도 괜찮을거같아요
+# ->네네
+  
+    
+#     이따가 자리 있으면 모일까요? 자리 없을거 같긴한데
+
+
+# 노션이랑 깃허브랑 셋팅 해놓고 기적인 이야기좀 해보면 좋을거 같아요
